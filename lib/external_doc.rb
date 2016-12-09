7e5ac5144d87aebf28ca98297b4250c307416c8e
Tijmen Brommet <tijmen@gmail.com>
2016-12-09 16:59:27 +0000

    Add docs for publishing-api
    This adds the documentation for the publishing-api docs.
    
    The list of pages to import is hard coded. I've considered using the
    GitHub API to fetch everything in the `/doc` directory. The downside to
    that is that if pages are renamed, we'll inadvertently break the pages
    here. Having a local list here allows us to set up redirects.

--- lib/external_doc.rb
+++ lib/external_doc.rb
@@ -5,6 +5,9 @@ class ExternalDoc
     # remove the own title of the page
     markdown = markdown.lines[2..-1].join.strip
 
+    # Make sure we link to the pages hosted here
+    markdown = markdown.gsub('.md', '.html')
+
     markdown
   end
 end
