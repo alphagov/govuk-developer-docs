c5bbe57c0a68d84b70c196b4558db70311c4ce0d
Tijmen Brommet <tijmen@gmail.com>
2016-12-14 12:59:55 +0000

    Remove refresh script
    This was used at one point to deploy this project. No longer needed.

--- run-refresh.sh
+++
@@ -1,6 +0,0 @@
-# Run the build
-bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec middleman build
-git add docs
-git commit -m "Refresh data from GitHub"
-git push origin master:master
