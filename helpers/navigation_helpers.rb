7fc42b9fcc7d66ba259d72b359b504883bc5fb56
Tijmen Brommet <tijmen@gmail.com>
2017-02-14 17:31:01 +0000

    Make sure menu highlights correctly
    This makes the main navigation elements highlight when the user is on
    that page and also when they're at a page that is a "child" of the main
    page. For example, this makes sure that "Apps" is highlighted when
    visiting `/apps/rummager.html`.

--- helpers/navigation_helpers.rb
+++ helpers/navigation_helpers.rb
@@ -1,5 +1,5 @@
 module NavigationHelpers
   def active_page?(page_path)
-    current_page.path.start_with?(page_path) || current_page.data.parent == page_path
+    ("/" + current_page.path) == page_path || current_page.data.parent == page_path
   end
 end
