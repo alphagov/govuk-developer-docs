9f400c4d288be78f83021c787b0541cf3ffe682b
Tijmen Brommet <tijmen@gmail.com>
2017-03-08 09:53:33 +0000

    Add govuk-lint
    And fix some minor issues. Will run on CI.

--- lib/requires.rb
+++ lib/requires.rb
@@ -2,4 +2,4 @@ require 'active_support/all'
 require 'tilt'
 require 'middleman-core'
 
-Dir[File.dirname(__FILE__) + '/**/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/**/*.rb'].each { |file| require file }
