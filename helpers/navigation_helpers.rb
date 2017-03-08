2d2f0d744f2afb08476c0eaf9e9e3911d51240fd
Tijmen Brommet <tijmen@gmail.com>
2017-03-08 09:53:33 +0000

    Highlight dashboard
    This doesn't work out of the box because `/` doesn't equals
    `/index.html`.

--- helpers/navigation_helpers.rb
+++ helpers/navigation_helpers.rb
@@ -1,5 +1,7 @@
 module NavigationHelpers
   def active_page?(page_path)
-    ("/" + current_page.path) == page_path || current_page.data.parent == page_path
+    (current_page.path == 'index.html' && page_path == '/') ||
+      "/" + current_page.path == page_path ||
+      current_page.data.parent == page_path
   end
 end
