7077e876e8b1c36962a893ed82a135e0bf0f3c10
Tijmen Brommet <tijmen@gmail.com>
2017-03-08 09:53:34 +0000

    Highlight active pages in sidebar

--- helpers/navigation_helpers.rb
+++ helpers/navigation_helpers.rb
@@ -4,4 +4,9 @@ module NavigationHelpers
       "/" + current_page.path == page_path ||
       current_page.data.parent == page_path
   end
+
+  def sidebar_link(name, page_path)
+    link_to name, page_path,
+      class: ("/" + current_page.path == page_path) ? 'toc-link--in-view' : nil
+  end
 end
