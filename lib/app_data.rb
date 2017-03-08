1e2714784e9a385869c9dc99a9d19955e28c58fb
Tijmen Brommet <tijmen@gmail.com>
2017-03-08 09:13:35 +0000

    Add example pages for apps
    This adds a list of pages published or rendered to the application pages.
    
    This data is fetched from the search API. Unfortunately most of the rendering_app/publishing_app is still missing, so not all pages will have the examples.

---
+++ lib/app_data.rb
@@ -0,0 +1,24 @@
+class AppData
+  SEARCH_URL = 'https://www.gov.uk/api/search.json?facet_publishing_app=100,examples:10,example_scope:global&facet_rendering_app=100,examples:10,example_scope:global&count=0'
+
+  def publishing_examples
+    extract_examples_from_search_result("publishing_app")
+  end
+
+  def rendering_examples
+    extract_examples_from_search_result("rendering_app")
+  end
+
+private
+
+  def result
+    @result ||= JSON.parse(Faraday.get(SEARCH_URL).body)
+  end
+
+  def extract_examples_from_search_result(facet_name)
+    result.dig("facets", facet_name, "options").reduce({}) do |hash, option|
+      hash[option.dig("value", "slug")] = option.dig("value", "example_info", "examples")
+      hash
+    end
+  end
+end
