967dbab1be187ffbae7f35139f30fa2e7c63686f
Tom Gladhill <whoojemaflip@gmail.com>
2017-04-06 15:29:39 +0100

    Add ownership by team to applications section

---
+++ app/applications_by_team.rb
@@ -0,0 +1,49 @@
+class ApplicationsByTeam
+  UNKNOWN = "unknown".freeze
+
+  def self.teams
+    applications.keys.reject { |team| team == UNKNOWN }
+  end
+
+  def self.[](team)
+    for_team(team)
+  end
+
+  def self.for_team(team)
+    applications.fetch(team, []).map(&:app_name)
+  end
+
+  def self.no_known_owner
+    applications[UNKNOWN].map(&:app_name)
+  end
+
+  def self.applications
+    @_applications ||=
+      YAML.load_file('data/applications.yml')
+        .map {|app_data| App.new(app_data) }
+        .reject(&:retired?)
+        .sort_by(&:app_name)
+        .group_by(&:team)
+  end
+
+  class App
+    attr_reader :app_data
+
+    def initialize(app_data)
+      @app_data = app_data
+    end
+
+    def retired?
+      app_data.fetch("retired", false)
+    end
+
+    def app_name
+      app_data.fetch("github_repo_name", UNKNOWN)
+    end
+
+    def team
+      app_data.fetch("team", UNKNOWN)
+    end
+
+  end
+end
