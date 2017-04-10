53aa5b43dd03ef39a24261134ecba9e9b86f20eb
Tijmen Brommet <tijmen@gmail.com>
2017-04-10 10:11:43 +0100

    Add test for `Supertypes` class

---
+++ spec/app/supertypes_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe Supertypes do
+  describe '.all' do
+    it 'works' do
+      stub_request(:get, "https://raw.githubusercontent.com/alphagov/govuk_document_types/master/data/supertypes.yml").
+        to_return(body: File.read("spec/fixtures/supertypes.yml"))
+
+      supertypes = Supertypes.all
+
+      expect(supertypes.first).to be_a(Supertypes::Supertype)
+    end
+  end
+end
