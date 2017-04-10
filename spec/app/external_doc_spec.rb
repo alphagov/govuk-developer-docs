315e732be89f9d433354397b740dceb6fb32ae6c
Tijmen Brommet <tijmen@gmail.com>
2017-04-10 10:11:43 +0100

    Add test for ExternalDoc class

---
+++ spec/app/external_doc_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe ExternalDoc do
+  describe '.fetch' do
+    it 'returns the markdown file without title' do
+      stub_request(:get, "https://example.org/markdown.md").
+        to_return(body: File.read("spec/fixtures/markdown.md"))
+
+      text = ExternalDoc.fetch('https://example.org/markdown.md')
+
+      expect(text).to eql('And some text')
+    end
+  end
+end
