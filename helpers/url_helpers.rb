a4fe35e44fbf1b48d80e217d9c119c8342609684
Tijmen Brommet <tijmen@gmail.com>
2018-08-31 12:43:53 +0100

    Use "contribution banner" feature from gem
    We have to copy-paste a little bit into `layout_with_sidebar`, because
    we're overriding that template so the HTML won't be added by default.
    We'll try to use the layout from the gem soon, which means we won't
    have to customise it anymore.

--- helpers/url_helpers.rb
+++ helpers/url_helpers.rb
@@ -2,12 +2,4 @@ module UrlHelpers
   def document_type_url(document_type_name)
     "/document-types/#{document_type_name}.html"
   end
-
-  def view_source_url
-    SourceUrl.new(locals, current_page).source_url
-  end
-
-  def report_issue_url
-    "https://github.com/alphagov/govuk-developer-docs/issues/new?labels=bug&title=Problem with '#{current_page.data.title}'&body=Problem with '#{current_page.data.title}' (#{config[:tech_docs][:host]}#{current_page.url})"
-  end
 end
