2c53c18d58a7fc8ca67c5864ec736e71043472e9
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-04-18 11:48:38 +0100

    allow # comments in code blocks

--- spec/pages/manual_page_spec.rb
+++ spec/pages/manual_page_spec.rb
@@ -33,8 +33,8 @@ Dir.glob("source/manual/**/*.md").each do |filename|
       source/manual/howto-merge-a-pull-request-from-an-external-contributor.html.md
     ])
       it "doesn't use H1 tags (the page title is already an H1)" do
-        expect(raw).not_to match(/\n#\s/), "This page contains an unnecessary H1." \
-          " This may have been triggered because of a # comment in a code block"
+        raw_without_code_blocks = raw.remove(/```[a-z]*\n[\s\S]*?\n```/) # want to allow `#` (comments) in code blocks
+        expect(raw_without_code_blocks).not_to match(/\n#\s/), "This page contains an unnecessary H1."
       end
     end
   end
