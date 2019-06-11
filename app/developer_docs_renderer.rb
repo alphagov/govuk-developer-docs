77f176ff84f4059af86d75ae3b09ade622f46cdf
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-06-11 14:14:27 +0100

    Adds 'breadcrumb' section link to top of pages
    Rather than duplicate the same text-to-id logic that is already duplicated
    in two places, I've encapsulate the logic in one place; 'string_to_id.rb'.
    
    Adds tests to cover some of the cases seen in our dev docs.

--- app/developer_docs_renderer.rb
+++ app/developer_docs_renderer.rb
@@ -1,19 +1,9 @@
+require_relative './string_to_id'
+
 class DeveloperDocsRenderer < GovukTechDocs::TechDocsHTMLRenderer
   def header(text, header_level)
-    anchor = githubify_fragment_id(text)
+    anchor = StringToId.convert(text)
     tag = "h" + header_level.to_s
     "<#{tag} id='#{anchor}'>#{text}</#{tag}>"
-  end
-
-  # Redcarpet uses a different algo to create fragment ids than github
-  # which has caused a TOC bug // ref https://trello.com/c/Re6fSBKj/24-change-internal-links-to-work-or-remove-internal-links
-  # this implementation modified for our purposes from version at jch/html-pipeline
-  # https://github.com/jch/html-pipeline/blob/master/lib/html/pipeline/toc_filter.rb
-  def githubify_fragment_id(text)
-    text
-      .downcase # lower case
-      .gsub(/<[^>]*>/, '') # crudely remove html tags
-      .gsub(/[^\w\- ]/, '') # remove any non-word characters
-      .tr(' ', '-') # replace spaces with hyphens
   end
 end
---
+++ app/string_to_id.rb
@@ -0,0 +1,14 @@
+class StringToId
+  # Redcarpet uses a different algo to create fragment ids than github
+  # which has caused a TOC bug // ref https://trello.com/c/Re6fSBKj/24-change-internal-links-to-work-or-remove-internal-links
+  # this implementation modified for our purposes from version at jch/html-pipeline
+  # https://github.com/jch/html-pipeline/blob/master/lib/html/pipeline/toc_filter.rb
+  def self.convert(string)
+    string
+      .downcase # lower case
+      .gsub(/<[^>]*>/, '') # crudely remove html tags
+      .gsub(/[^\w\-\. ]/, '') # remove any non-word, non-hyphen & non-period characters
+      .gsub(/[ \.]/, '-') # replace spaces & periods with hyphens
+      .gsub(/-{2,}/, '-') # replace two (or more) subsequent hyphens with single hyphens
+  end
+end
