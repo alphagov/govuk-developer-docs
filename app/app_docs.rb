e1bbc86e9b29a52e20f661c3a6b50c9e272c21c6
Issy Long <me@issyl0.co.uk>
2019-11-29 14:53:18 +0000

    Licensing is in AWS now!
    - Licensing was the last thing in UK Cloud, so we can remove the docs
      about that specific VPN, and the mapping of "ukcloud" to "UK Cloud".

--- app/app_docs.rb
+++ app/app_docs.rb
@@ -3,7 +3,6 @@ class AppDocs
     "aws" => "AWS",
     "paas" => "GOV.UK PaaS",
     "carrenza" => "Carrenza",
-    "ukcloud" => "UK Cloud",
     "heroku" => "Heroku",
     "none" => "None",
   }.freeze
