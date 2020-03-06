38ef5abfe82d64dfeca284ddfcec4c307b68e0af
Issy Long <me@issyl0.co.uk>
2020-03-06 12:37:15 +0000

    Fix Lint/NonDeterministicRequireOrder and Style/StringLiterals offenses

--- app/requires.rb
+++ app/requires.rb
@@ -6,5 +6,5 @@ require "json"
 
 CACHE = ActiveSupport::Cache::FileStore.new(".cache")
 
-Dir[File.dirname(__FILE__) + "/../lib/*.rb"].each { |file| require file }
-Dir[File.dirname(__FILE__) + "/**/*.rb"].each { |file| require file }
+Dir[File.dirname(__FILE__) + "/../lib/*.rb"].sort.each { |file| require file }
+Dir[File.dirname(__FILE__) + "/**/*.rb"].sort.each { |file| require file }
