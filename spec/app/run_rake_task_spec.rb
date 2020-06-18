d39fb154cc33cd07119d7d7c2065d08d3e4aa5a7
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-06-18 12:32:32 +0100

    aws migration publishers
    With the AWS migration of publishers, we need to change the location of the publishers to AWS in the developer docs.

--- spec/app/run_rake_task_spec.rb
+++ spec/app/run_rake_task_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe RunRakeTask do
 
     describe "given an application name" do
       before do
-        stub_request(:get, "https://raw.githubusercontent.com/alphagov/govuk-puppet/master/hieradata/common.yaml")
+        stub_request(:get, "https://raw.githubusercontent.com/alphagov/govuk-puppet/master/hieradata_aws/common.yaml")
           .to_return(body: File.read("spec/fixtures/puppet-hieradata-common.yaml"))
       end
 
@@ -34,8 +34,8 @@ RSpec.describe RunRakeTask do
 
       it "has three links" do
         expect(html).to have_link("Run publishing_api:republish on Integration", href: "https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=publishing_api:republish")
-        expect(html).to have_link("Run publishing_api:republish on Staging", href: "https://deploy.staging.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=publishing_api:republish")
-        expect(html).to have_link("Run publishing_api:republish on Production", href: "https://deploy.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=publishing_api:republish")
+        expect(html).to have_link("Run publishing_api:republish on Staging", href: "https://deploy.blue.staging.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=publishing_api:republish")
+        expect(html).to have_link("Run publishing_api:republish on Production", href: "https://deploy.blue.production.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=publishing_api:republish")
       end
     end
   end
