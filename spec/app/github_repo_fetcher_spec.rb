e2e49969fbe8aec24a3fc81f3cbad47762b6b203
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-09-21 14:52:23 +0100

    Add 'last updated' banner to external docs (based on commit SHA)
    Unlike the 'internal' docs that live in govuk-developer-docs,
    these externally pulled in docs have no 'frontmatter' (YAML)
    describing their state. All we have to go on is the commit history
    itself.
    
    This commit creates a 'latest_commit' property that is proxied
    through to the frontend via ProxyPages. We use this to show
    the 'last updated' value and link to the SHA of the commit so
    that the viewer can see the latest changes.

--- spec/app/github_repo_fetcher_spec.rb
+++ spec/app/github_repo_fetcher_spec.rb
@@ -82,39 +82,42 @@ RSpec.describe GitHubRepoFetcher do
   end
 
   describe "#docs" do
+    let(:repo_name) { SecureRandom.uuid }
+
     def docs_url(repo_name)
       "https://api.github.com/repos/alphagov/#{repo_name}/contents/docs"
     end
 
     it "returns an array of hashes including title derived from markdown contents" do
-      repo_name = SecureRandom.uuid
       markdown_url = "https://raw.githubusercontent.com/alphagov/#{repo_name}/master/docs/analytics.md"
       source_url = "https://github.com/alphagov/#{repo_name}/blob/master/docs/analytics.md"
       markdown_fixture = "# Analytics \n Foo"
-      api_response = [
-        {
-          name: "analytics.md",
-          download_url: markdown_url,
-          html_url: source_url,
-        },
-      ]
-      expected_output = [
+      path = "docs/analytics.md"
+      default_branch = "main"
+      latest_commit = { sha: SecureRandom.hex(40), timestamp: Time.now.utc.to_s }
+      doc_response = [{ name: "analytics.md", path: path, download_url: markdown_url, html_url: source_url }]
+      commit_response = [{ sha: latest_commit[:sha], commit: { author: { date: latest_commit[:timestamp] } } }]
+
+      allow(GitHubRepoFetcher.instance).to receive(:repo)
+        .with(repo_name) { OpenStruct.new(default_branch: default_branch) }
+      stub_request(:get, docs_url(repo_name))
+        .to_return(body: doc_response.to_json, headers: { content_type: "application/json" })
+      stub_request(:get, "https://api.github.com/repos/alphagov/#{repo_name}/commits?path=#{path}&per_page=100&sha=#{default_branch}")
+        .to_return(body: commit_response.to_json, headers: { content_type: "application/json" })
+      stub_request(:get, markdown_url).to_return(body: markdown_fixture)
+
+      expect(GitHubRepoFetcher.instance.docs(repo_name)).to eq([
         {
           title: "Analytics",
           path: "/apis/#{repo_name}/analytics.html",
           markdown: markdown_fixture,
           source_url: source_url,
+          latest_commit: latest_commit,
         },
-      ]
-      stub_request(:get, docs_url(repo_name))
-        .to_return(body: api_response.to_json, headers: { content_type: "application/json" })
-      stub_request(:get, markdown_url).to_return(body: markdown_fixture)
-
-      expect(GitHubRepoFetcher.instance.docs(repo_name)).to eq(expected_output)
+      ])
     end
 
     it "skips over any non-markdown files" do
-      repo_name = SecureRandom.uuid
       api_response = [
         {
           "name": "digests.png",
@@ -128,7 +131,6 @@ RSpec.describe GitHubRepoFetcher do
     end
 
     it "returns nil if no docs folder exists" do
-      repo_name = SecureRandom.uuid
       stub_request(:get, docs_url(repo_name))
         .to_return(status: 404, body: "{}", headers: { content_type: "application/json" })
 
