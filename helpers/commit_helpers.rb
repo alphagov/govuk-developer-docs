35a60afe4a4fd05146772353f7b6654cc4b3efcd
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-10-12 17:17:19 +0100

    Share same partial for internal/external 'last updated' banner
    We want the banner to look the same regardless of where the doc
    is hosted. Therefore I've moved the HTML into a shared partial
    that is called by the respective layouts.
    
    One caveat is that the external docs are 'unnecessarily' being
    passed through the CommitHelpers, but it seems reasonable to allow
    for this with a conditional. But the advantage of this is that we
    no longer need the conditional 'guard' for /apis.html. The
    commit-retrieval mechanism is more robust - in this case, it now
    fetches the commit SHA for the local /apis.html.md file.

--- helpers/commit_helpers.rb
+++ helpers/commit_helpers.rb
@@ -1,10 +1,14 @@
 module CommitHelpers
   def commit_url(current_page)
+    return "https://github.com/alphagov/#{current_page.data.app_name}/commit/#{current_page.data.latest_commit[:sha]}" if current_page.data.latest_commit
+
     commit_sha = `git log -1 --format=oneline #{source_file(current_page)} | cut -d ' ' -f 1`.strip
     "https://github.com/alphagov/govuk-developer-docs/commit/#{commit_sha}"
   end
 
   def last_updated(current_page)
+    return current_page.data.latest_commit[:timestamp] if current_page.data.latest_commit
+
     `TZ=UTC git log -1 --date='format-local:%Y-%m-%d %T UTC' --format="%cd" #{source_file(current_page)}`.strip
   end
 
--- spec/helpers/commit_helpers_spec.rb
+++ spec/helpers/commit_helpers_spec.rb
@@ -5,10 +5,25 @@ RSpec.describe CommitHelpers do
   let(:helper) { Class.new { extend CommitHelpers } }
 
   describe "#commit_url" do
+    it "returns the commit_url associated with the page data, if that exists" do
+      app_name = "some-repo"
+      commit_sha = SecureRandom.hex(40)
+      current_page = OpenStruct.new(data: OpenStruct.new(
+        app_name: app_name,
+        latest_commit: {
+          sha: commit_sha,
+        },
+      ))
+      expect(helper.commit_url(current_page)).to eq("https://github.com/alphagov/#{app_name}/commit/#{commit_sha}")
+    end
+
     it "returns commit URL for the commit associated with the source file of current_page" do
       source_file = "foo/bar.md"
       commit_sha = SecureRandom.hex(40)
-      current_page = OpenStruct.new(file_descriptor: OpenStruct.new(relative_path: source_file))
+      current_page = OpenStruct.new(
+        data: OpenStruct.new,
+        file_descriptor: OpenStruct.new(relative_path: source_file),
+      )
       allow(helper).to receive(:`).and_return(commit_sha)
       expect(helper.commit_url(current_page))
         .to eq("https://github.com/alphagov/govuk-developer-docs/commit/#{commit_sha}")
@@ -16,9 +31,20 @@ RSpec.describe CommitHelpers do
   end
 
   describe "#last_updated" do
-    it "returns a datetime string of the form 'YYYY-MM-DD HH:mm:ss UTC'" do
+    it "returns the commit timestamp associated with the (remote) page data, if that exists" do
+      last_committed = "2020-09-03 09:53:56 UTC"
+      current_page = OpenStruct.new(data: OpenStruct.new(
+        latest_commit: { timestamp: last_committed },
+      ))
+      expect(helper.last_updated(current_page)).to eq(last_committed)
+    end
+
+    it "returns a datetime string of the form 'YYYY-MM-DD HH:mm:ss UTC' for the local file" do
       source_file = "index.html.erb"
-      current_page = OpenStruct.new(file_descriptor: OpenStruct.new(relative_path: source_file))
+      current_page = OpenStruct.new(
+        data: OpenStruct.new,
+        file_descriptor: OpenStruct.new(relative_path: source_file),
+      )
       expect(helper.last_updated(current_page)).to match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC/)
     end
   end
