e1e02eeaa86e356fc8fd4745ad4b914c4aae4052
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-10-12 18:19:43 +0100

    Format the 'Last updated' field
    I've opted for the same format as GitHub uses for older dates.
    We could in future make this more clever (like GitHub does) by
    omitting the year if it is the current year, and/or by making
    recent updates relative (e.g. "3 days ago").

--- spec/helpers/commit_helpers_spec.rb
+++ spec/helpers/commit_helpers_spec.rb
@@ -31,21 +31,21 @@ RSpec.describe CommitHelpers do
   end
 
   describe "#last_updated" do
-    it "returns the commit timestamp associated with the (remote) page data, if that exists" do
+    it "formats the commit timestamp associated with the (remote) page data, if that exists" do
       last_committed = "2020-09-03 09:53:56 UTC"
       current_page = OpenStruct.new(data: OpenStruct.new(
         latest_commit: { timestamp: last_committed },
       ))
-      expect(helper.last_updated(current_page)).to eq(last_committed)
+      expect(helper.last_updated(current_page)).to eq("3 Sep 2020")
     end
 
-    it "returns a datetime string of the form 'YYYY-MM-DD HH:mm:ss UTC' for the local file" do
+    it "formats the commit date of the local file if no page data exists" do
       source_file = "index.html.erb"
       current_page = OpenStruct.new(
         data: OpenStruct.new,
         file_descriptor: OpenStruct.new(relative_path: source_file),
       )
-      expect(helper.last_updated(current_page)).to match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC/)
+      expect(helper.last_updated(current_page)).to match(/\d{1,2} \w+ \d{4}/)
     end
   end
 end
