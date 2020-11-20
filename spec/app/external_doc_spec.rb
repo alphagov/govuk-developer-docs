9818b165365f35a7b5afb3c65fc7c65b71a81f96
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-11-20 16:59:40 +0000

    Allow `ExternalDoc.parse` to take `repository`/`path` parameters
    This allows us to make links and images relative.
    I've copied the tests from the `.fetch` method.
    In the next commit I'll strip these out of the `.fetch` method,
    following the sole responsibility principle.

--- spec/app/external_doc_spec.rb
+++ spec/app/external_doc_spec.rb
@@ -67,6 +67,50 @@ RSpec.describe ExternalDoc do
       expected_html = "<p>These curly quotes “make commonmarker throw an exception”</p>"
       expect(described_class.parse(markdown).to_s).to eq(expected_html)
     end
+
+    context "when passed a repository and path" do
+      subject(:html) do
+        Capybara.string(described_class.parse(
+          File.read("spec/fixtures/markdown.md"),
+          repository: "lipsum",
+          path: "markdown.md",
+        ).to_s)
+      end
+
+      it "removes the title of the page" do
+        expect(html).not_to have_selector("h1", text: "Lorem ipsum")
+      end
+
+      it "rewrites links to markdown pages" do
+        expect(html).to have_link("turpis ultrices", href: "/ultrices.html")
+      end
+
+      it "does not rewrite links to markdown pages with a host" do
+        expect(html).to have_link("Nam eget dui", href: "https://nam.com/eget/dui/uploading.md")
+        expect(html).not_to have_link("Nam eget dui", href: "https://nam.com/eget/dui/uploading.html")
+      end
+
+      it "rewrites relative images" do
+        expect(html).to have_css('img[src="https://raw.githubusercontent.com/alphagov/lipsum/master/suspendisse_iaculis.png"]')
+      end
+
+      it "rewrites relative URLs" do
+        expect(html).to have_link("tincidunt leo", href: "https://github.com/alphagov/lipsum/blob/master/lib/tincidunt_leo.rb")
+      end
+
+      it "maintains anchor links" do
+        expect(html).to have_link("Suspendisse iaculis", href: "#suspendisse-iaculis")
+      end
+
+      it "adds an id attribute to all headers so they can be accessed from a table of contents" do
+        expect(html).to have_selector("h2#tldr")
+      end
+
+      it "converts heading IDs properly" do
+        expect(html).to have_selector("h3#data-gov-uk")
+        expect(html).to have_selector("h3#patterns-style-guides")
+      end
+    end
   end
 
   describe ".title" do
