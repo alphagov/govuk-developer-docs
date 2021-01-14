73b1700628db95bdb21742f740759ce6f994c310
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-01-14 15:21:36 +0000

    Move `App` into its own file
    Having the `App` class nested inside `AppDocs` made it harder to
    test, and a much longer file to read and understand. The
    functionality of both the `AppDocs` and `App` classes has now
    grown to the point where it makes sense to treat them as disparate
    classes in their own right.

--- spec/app/app_docs_spec.rb
+++ spec/app/app_docs_spec.rb
@@ -1,7 +1,7 @@
-RSpec.describe AppDocs::App do
+RSpec.describe AppDocs do
   before :each do
     allow(AppDocs).to receive(:apps) do
-      applications.map(&:stringify_keys).map { |app_data| AppDocs::App.new(app_data) }
+      applications.map(&:stringify_keys).map { |app_data| App.new(app_data) }
     end
   end
 
@@ -17,20 +17,6 @@ RSpec.describe AppDocs::App do
       { github_repo_name: "app-on-paas", production_hosted_on: "paas" },
       { github_repo_name: "some-retired-application" },
     ]
-  end
-
-  describe "production_url" do
-    it "has a good default" do
-      app = AppDocs::App.new("type" => "Publishing app", "github_repo_name" => "my-app")
-
-      expect(app.production_url).to eql("https://my-app.publishing.service.gov.uk")
-    end
-
-    it "allows override" do
-      app = AppDocs::App.new("type" => "Publishing app", "production_url" => "something else")
-
-      expect(app.production_url).to eql("something else")
-    end
   end
 
   describe "hosters_descending" do
@@ -49,7 +35,7 @@ RSpec.describe AppDocs::App do
   describe "apps_on_host" do
     it "should return apps hosted on the named host" do
       paas_app = AppDocs.apps_on_host("paas").first
-      expect(paas_app).to be_an(AppDocs::App)
+      expect(paas_app).to be_an(App)
       expect(paas_app.app_name).to eq("app-on-paas")
     end
 
@@ -67,123 +53,6 @@ RSpec.describe AppDocs::App do
         govuk-frontend
         whitehall
       ])
-    end
-  end
-
-  describe "aws_puppet_class" do
-    before do
-      app_data = {
-        "calculators_frontend" => {
-          "apps" => %w[
-            calculators
-            finder-frontend
-            licencefinder
-            smartanswers
-          ],
-        },
-      }
-      allow(AppDocs).to receive(:aws_machines).and_return(app_data)
-    end
-
-    it "should find puppet class via github repo name if neither app name nor puppet name provided" do
-      expect(AppDocs::App.new("github_repo_name" => "calculators").aws_puppet_class).to eq("calculators_frontend")
-    end
-
-    it "should find puppet class via app name" do
-      expect(AppDocs::App.new("app_name" => "calculators").aws_puppet_class).to eq("calculators_frontend")
-    end
-
-    it "should find puppet class via puppet name" do
-      expect(AppDocs::App.new("puppet_name" => "smartanswers", "github_repo_name" => "foo").aws_puppet_class).to eq("calculators_frontend")
-    end
-
-    it "should return error message if no puppet class found" do
-      expect(AppDocs::App.new("github_repo_name" => "foo").aws_puppet_class)
-        .to eq("Unknown - have you configured and merged your app in govuk-puppet/hieradata_aws/common.yaml")
-    end
-  end
-
-  describe "dashboard_url" do
-    let(:production_hosted_on) { nil }
-    let(:app) do
-      described_class.new(
-        "type" => "Publishing app",
-        "github_repo_name" => "my-app",
-        "production_hosted_on" => production_hosted_on,
-      )
-    end
-    subject(:dashboard_url) { app.dashboard_url }
-
-    describe "hosted on AWS" do
-      let(:production_hosted_on) { "aws" }
-      it { is_expected.to eql("https://grafana.production.govuk.digital/dashboard/file/my-app.json") }
-    end
-
-    describe "hosted on Carrenza" do
-      let(:production_hosted_on) { "carrenza" }
-      it { is_expected.to eql("https://grafana.publishing.service.gov.uk/dashboard/file/my-app.json") }
-    end
-  end
-
-  describe "rake_task_url" do
-    let(:production_hosted_on) { nil }
-    let(:environment) { nil }
-    let(:rake_task) { "" }
-    let(:app) do
-      described_class.new(
-        "github_repo_name" => "content-publisher",
-        "machine_class" => "backend",
-        "production_hosted_on" => production_hosted_on,
-      )
-    end
-    subject(:dashboard_url) { app.rake_task_url(environment, rake_task) }
-
-    describe "hosted on AWS and environment is production" do
-      let(:production_hosted_on) { "aws" }
-      let(:environment) { "production" }
-
-      it { is_expected.to eql("https://deploy.blue.production.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
-
-      describe "with a Rake task" do
-        let(:rake_task) { "task" }
-        it { is_expected.to eql("https://deploy.blue.production.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
-      end
-    end
-
-    describe "hosted on AWS and environment is integration" do
-      let(:production_hosted_on) { "aws" }
-      let(:environment) { "integration" }
-
-      it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
-
-      describe "with a Rake task" do
-        let(:rake_task) { "task" }
-        it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
-      end
-    end
-
-    describe "hosted on Carrenza and environment is production" do
-      let(:production_hosted_on) { "carrenza" }
-      let(:environment) { "production" }
-
-      it { is_expected.to eql("https://deploy.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
-
-      describe "with a Rake task" do
-        let(:rake_task) { "task" }
-        it { is_expected.to eql("https://deploy.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
-      end
-    end
-
-    describe "hosted on Carrenza and environment is integration" do
-      let(:production_hosted_on) { "carrenza" }
-      let(:environment) { "integration" }
-
-      it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
-
-      describe "with a Rake task" do
-        let(:rake_task) { "task" }
-        it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
-      end
     end
   end
 end
---
+++ spec/app/app_spec.rb
@@ -0,0 +1,132 @@
+RSpec.describe App do
+  describe "production_url" do
+    it "has a good default" do
+      app = App.new("type" => "Publishing app", "github_repo_name" => "my-app")
+
+      expect(app.production_url).to eql("https://my-app.publishing.service.gov.uk")
+    end
+
+    it "allows override" do
+      app = App.new("type" => "Publishing app", "production_url" => "something else")
+
+      expect(app.production_url).to eql("something else")
+    end
+  end
+
+  describe "aws_puppet_class" do
+    before do
+      app_data = {
+        "calculators_frontend" => {
+          "apps" => %w[
+            calculators
+            finder-frontend
+            licencefinder
+            smartanswers
+          ],
+        },
+      }
+      allow(AppDocs).to receive(:aws_machines).and_return(app_data)
+    end
+
+    it "should find puppet class via github repo name if neither app name nor puppet name provided" do
+      expect(App.new("github_repo_name" => "calculators").aws_puppet_class).to eq("calculators_frontend")
+    end
+
+    it "should find puppet class via app name" do
+      expect(App.new("app_name" => "calculators").aws_puppet_class).to eq("calculators_frontend")
+    end
+
+    it "should find puppet class via puppet name" do
+      expect(App.new("puppet_name" => "smartanswers", "github_repo_name" => "foo").aws_puppet_class).to eq("calculators_frontend")
+    end
+
+    it "should return error message if no puppet class found" do
+      expect(App.new("github_repo_name" => "foo").aws_puppet_class)
+        .to eq("Unknown - have you configured and merged your app in govuk-puppet/hieradata_aws/common.yaml")
+    end
+  end
+
+  describe "dashboard_url" do
+    let(:production_hosted_on) { nil }
+    let(:app) do
+      described_class.new(
+        "type" => "Publishing app",
+        "github_repo_name" => "my-app",
+        "production_hosted_on" => production_hosted_on,
+      )
+    end
+    subject(:dashboard_url) { app.dashboard_url }
+
+    describe "hosted on AWS" do
+      let(:production_hosted_on) { "aws" }
+      it { is_expected.to eql("https://grafana.production.govuk.digital/dashboard/file/my-app.json") }
+    end
+
+    describe "hosted on Carrenza" do
+      let(:production_hosted_on) { "carrenza" }
+      it { is_expected.to eql("https://grafana.publishing.service.gov.uk/dashboard/file/my-app.json") }
+    end
+  end
+
+  describe "rake_task_url" do
+    let(:production_hosted_on) { nil }
+    let(:environment) { nil }
+    let(:rake_task) { "" }
+    let(:app) do
+      described_class.new(
+        "github_repo_name" => "content-publisher",
+        "machine_class" => "backend",
+        "production_hosted_on" => production_hosted_on,
+      )
+    end
+    subject(:dashboard_url) { app.rake_task_url(environment, rake_task) }
+
+    describe "hosted on AWS and environment is production" do
+      let(:production_hosted_on) { "aws" }
+      let(:environment) { "production" }
+
+      it { is_expected.to eql("https://deploy.blue.production.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
+
+      describe "with a Rake task" do
+        let(:rake_task) { "task" }
+        it { is_expected.to eql("https://deploy.blue.production.govuk.digital/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
+      end
+    end
+
+    describe "hosted on AWS and environment is integration" do
+      let(:production_hosted_on) { "aws" }
+      let(:environment) { "integration" }
+
+      it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
+
+      describe "with a Rake task" do
+        let(:rake_task) { "task" }
+        it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
+      end
+    end
+
+    describe "hosted on Carrenza and environment is production" do
+      let(:production_hosted_on) { "carrenza" }
+      let(:environment) { "production" }
+
+      it { is_expected.to eql("https://deploy.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
+
+      describe "with a Rake task" do
+        let(:rake_task) { "task" }
+        it { is_expected.to eql("https://deploy.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
+      end
+    end
+
+    describe "hosted on Carrenza and environment is integration" do
+      let(:production_hosted_on) { "carrenza" }
+      let(:environment) { "integration" }
+
+      it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=") }
+
+      describe "with a Rake task" do
+        let(:rake_task) { "task" }
+        it { is_expected.to eql("https://deploy.integration.publishing.service.gov.uk/job/run-rake-task/parambuild/?TARGET_APPLICATION=content-publisher&MACHINE_CLASS=backend&RAKE_TASK=task") }
+      end
+    end
+  end
+end
