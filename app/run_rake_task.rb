2fcbdddeeec946ed3a62d7c8e73f3858d95d44a5
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-02-08 12:38:53 +0000

    Add `terse_links` method for RunRakeTask
    This is to be displayed on one line on the Application template.
    This would worsen the accessibility of the links, so I've added
    an aria label to each link so that they make sense in isolation
    when read by screen readers.

--- app/run_rake_task.rb
+++ app/run_rake_task.rb
@@ -23,6 +23,24 @@ class RunRakeTask
     ERB
   end
 
+  def self.terse_links(application, rake_task = "")
+    application = find_application(application) if application.is_a? String
+
+    rake_task_name = rake_task.presence || "a Rake task"
+
+    integration_link = "<a
+      href=\"#{application.rake_task_url('integration', rake_task)}\"
+      aria-label=\"Run #{rake_task_name} on Integration\">Integration</a>"
+    staging_link = "<a
+      href=\"#{application.rake_task_url('staging', rake_task)}\"
+      aria-label=\"Run #{rake_task_name} on Staging\">Staging</a>"
+    production_link = "<a
+      href=\"#{application.rake_task_url('production', rake_task)}\"
+      aria-label=\"Run #{rake_task_name} on Production\">⚠️ Production ⚠️</a>"
+
+    "<span aria-hidden='true'>Run #{rake_task_name} on</span> #{integration_link}, #{staging_link} or #{production_link}"
+  end
+
   def self.find_application(name)
     Applications.all.select { |app| app.app_name == name }.first
   end
