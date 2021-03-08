7b15880df8933a3f0a2b5340761382dbf62a1991
Kevin Dew <kevindew@me.com>
2021-03-08 19:26:54 +0000

    Auto-correct most rubocop violations
    rubocop -A

--- app/developer_docs_renderer.rb
+++ app/developer_docs_renderer.rb
@@ -3,7 +3,7 @@ require_relative "./string_to_id"
 class DeveloperDocsRenderer < GovukTechDocs::TechDocsHTMLRenderer
   def header(text, header_level)
     anchor = StringToId.convert(text)
-    tag = "h" + header_level.to_s
+    tag = "h#{header_level}"
     "<#{tag} id='#{anchor}'>#{text}</#{tag}>"
   end
 end
--- app/string_to_id.rb
+++ app/string_to_id.rb
@@ -3,8 +3,8 @@ class StringToId
     string
       .downcase # lower case
       .gsub(/<[^>]*>/, "") # crudely remove html tags
-      .gsub(/[^\w\-\. ]/, "") # remove any non-word, non-hyphen & non-period characters
-      .gsub(/[ \.]/, "-") # replace spaces & periods with hyphens
+      .gsub(/[^\w\-. ]/, "") # remove any non-word, non-hyphen & non-period characters
+      .gsub(/[ .]/, "-") # replace spaces & periods with hyphens
       .gsub(/-{2,}/, "-") # replace two (or more) subsequent hyphens with single hyphens
   end
 end
