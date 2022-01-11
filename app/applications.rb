1a43a73cfc2e009b9cd5a101676109a4464ac1ba
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-01-11 13:37:36 +0000

    Encapsulate Hosts information inside new Hosts class
    The 'Applications' class was doing a bit too much. 'Applications'
    should purely be a way of selecting differently scoped lists of
    applications. 'Hosts' knows about which cloud providers exist, and
    how to retrieve AWS Puppet node names.
    
    Keeping these separate will make it easier to make further
    refactors in future (such as moving towards agnostic 'Repository'
    concept, rather than just 'App').

--- app/applications.rb
+++ app/applications.rb
@@ -1,11 +1,4 @@
 class Applications
-  HOSTERS = {
-    "aws" => "AWS",
-    "paas" => "GOV.UK PaaS",
-    "heroku" => "Heroku",
-    "none" => "None",
-  }.freeze
-
   def self.all
     @all ||= YAML.load_file("data/applications.yml").map do |app_data|
       App.new(app_data)
@@ -20,25 +13,7 @@ class Applications
     Applications.all.reject(&:retired?).sort_by(&:app_name)
   end
 
-  def self.hosters_descending
-    ordered_keys = HOSTERS.keys.sort do |a, b|
-      [on_host(b).count, a] <=> [on_host(a).count, b]
-    end
-    ordered_keys.map { |key| [key, HOSTERS[key]] }.to_h
-  end
-
-  def self.on_host(host)
-    Applications.all
-      .select { |app| app.production_hosted_on == host }
-      .sort_by(&:app_name)
-  end
-
   def self.app_data
     @app_data ||= AppData.new
-  end
-
-  def self.aws_machines
-    @common_aws ||= HTTP.get_yaml("https://raw.githubusercontent.com/alphagov/govuk-puppet/master/hieradata_aws/common.yaml")
-    @common_aws["node_class"]
   end
 end
--- spec/app/applications_spec.rb
+++ spec/app/applications_spec.rb
@@ -5,18 +5,6 @@ RSpec.describe Applications do
     end
   end
 
-  let(:applications) do
-    [
-      { github_repo_name: "whitehall", production_hosted_on: "aws" },
-      { github_repo_name: "asset-manager", production_hosted_on: "aws" },
-      { github_repo_name: "content-store", production_hosted_on: "aws" },
-      { github_repo_name: "govuk-frontend", production_hosted_on: "aws" },
-      { github_repo_name: "app-on-heroku", production_hosted_on: "heroku" },
-      { github_repo_name: "app-on-paas", production_hosted_on: "paas" },
-      { github_repo_name: "some-retired-application" },
-    ]
-  end
-
   describe "public" do
     let(:applications) do
       [
@@ -40,41 +28,6 @@ RSpec.describe Applications do
 
     it "should return only apps that are not retired" do
       expect(Applications.active.map(&:github_repo_name)).to eq(%w[asset-manager])
-    end
-  end
-
-  describe "hosters_descending" do
-    it "should return hosters in descending order of repo count" do
-      expect(Applications.hosters_descending.keys).to eq(%w[
-        aws
-        heroku
-        paas
-        none
-      ])
-    end
-  end
-
-  describe "on_host" do
-    it "should return apps hosted on the named host" do
-      paas_app = Applications.on_host("paas").first
-      expect(paas_app).to be_an(App)
-      expect(paas_app.app_name).to eq("app-on-paas")
-    end
-
-    it "should return all apps in production" do
-      production_apps_count = applications.count { |app| app[:production_hosted_on].present? }
-      apps_by_host = Applications::HOSTERS.map { |key, _val| Applications.on_host(key) }.flatten
-      expect(apps_by_host.count).to eq(production_apps_count)
-    end
-
-    it "should return apps in alphabetical order" do
-      apps_on_aws = Applications.on_host("aws")
-      expect(apps_on_aws.map(&:app_name)).to eq(%w[
-        asset-manager
-        content-store
-        govuk-frontend
-        whitehall
-      ])
     end
   end
 end
