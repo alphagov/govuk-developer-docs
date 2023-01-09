6371bc4300eb73e4283abdbad47fe5467e310495
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-01-09 14:40:47 +0000

    Pass "alias" keyword to YAML.load
    Fixes error:
    
    ```
    Psych::BadAlias: Unknown alias: node_class
    ```
    
    Ruby 3.1 comes with Psych 4, which has a major breaking change:
    
    The new YAML loading methods (Psych 4) do not load aliases unless they get the aliases: true argument.
    The old YAML loading methods (Psych 3) do not support the aliases keyword.
    
    See: https://stackoverflow.com/a/71192990

--- spec/data/repos_spec.rb
+++ spec/data/repos_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "repos.yml" do
   describe "repos.yml" do
-    let!(:repos) { YAML.load_file("data/repos.yml") }
+    let!(:repos) { YAML.load_file("data/repos.yml", aliases: true) }
 
     it "lists each repository in alphabetical order" do
       expect(repos.pluck("repo_name")).to eq(repos.pluck("repo_name").sort)
