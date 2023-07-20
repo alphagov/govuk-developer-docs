506b22d0540d0a15d7f9bb635a373499fc3856a2
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2023-07-20 13:59:29 +0100

    Link to team channels from repo pages
    This adds a hyperlink to the team Slack channel names so they can be
    clicked on.

--- helpers/url_helpers.rb
+++ helpers/url_helpers.rb
@@ -2,4 +2,8 @@ module UrlHelpers
   def document_type_url(document_type_name)
     "/document-types/#{document_type_name}.html"
   end
+
+  def slack_url(channel_name)
+    "https://gds.slack.com/channels/#{channel_name.sub('#', '')}"
+  end
 end
