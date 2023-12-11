c9a1a684ea2a1e8faa4fd8db9232717828f4efc2
PeterHattyar <peter.hattyar@digital.cabinet-office.gov.uk>
2023-12-11 04:58:12 +0000

    gems.json endpoint
    Created endpoint to list all GOV.UK gem repositories,
    in the same fashion as the already existing apps.json and repos.json
    
    This is to aid the gem-release-alert application.

--- spec/app/repos_spec.rb
+++ spec/app/repos_spec.rb
@@ -73,4 +73,17 @@ RSpec.describe Repos do
       expect(Repos.active_apps.map(&:repo_name)).to eq(%w[asset-manager])
     end
   end
+
+  describe "active_gems" do
+    let(:repos) do
+      [
+        { repo_name: "cache-clearing-service", type: "Services" },
+        { repo_name: "gds-api-adapters", type: "Gems" },
+      ]
+    end
+
+    it "should return only apps that are classified as gems" do
+      expect(Repos.active_gems.map(&:repo_name)).to eq(%w[gds-api-adapters])
+    end
+  end
 end
