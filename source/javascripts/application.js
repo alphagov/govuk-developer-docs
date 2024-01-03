adc568335844bb8de669ba1a465358375627c93c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-01-03 17:02:07 +0000

    Add new cookie banner partials and code
    - cookie_banner uses the govuk_publishing_components cookie banner component, with custom text
    - cookie_wrapper imports that markup as a JS variable to be inserted into an element and prepended to the body, to get around the problem of the layout not being editable
    - include cookie banner code from govuk_publishing_components

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,5 @@
 //= require govuk_tech_docs
+//= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib/trigger-event
+//= require govuk_publishing_components/lib/cookie-functions
+//= require govuk_publishing_components/components/cookie-banner
