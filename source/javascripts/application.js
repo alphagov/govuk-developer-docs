3cb521e3578d9abb206ebb11eb253cf08a43411c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-01-04 10:46:45 +0000

    Include GA4 code from gem

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,3 +3,4 @@
 //= require govuk_publishing_components/lib/trigger-event
 //= require govuk_publishing_components/lib/cookie-functions
 //= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/load-analytics
