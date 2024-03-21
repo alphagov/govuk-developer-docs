f2083342e7ecae105f8ca4b30b937209e2b6044f
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2024-03-21 22:12:51 +0000

    Make StringToId more robust to arbitrary input.
    - Add some tests.
    - Don't "crudely remove HTML tags"; do it properly. Even though this is
      only gonna see semi-trusted input, it's just not a good look to be
      rolling our own with that.
    - Remove comments that just give false reassurance.
    - Prefer simpler string functions over regex where appropriate.
    
    Fixes https://github.com/alphagov/govuk-developer-docs/security/code-scanning/2.
    
    Gemfile.lock changes were generated by `bundle install`.

--- app/string_to_id.rb
+++ app/string_to_id.rb
@@ -1,10 +1,12 @@
+require "sanitize"
+
 class StringToId
   def self.convert(string)
-    string
-      .downcase # lower case
-      .gsub(/<[^>]*>/, "") # crudely remove html tags
-      .gsub(/[^\w\-. ]/, "") # remove any non-word, non-hyphen & non-period characters
-      .gsub(/[ .]/, "-") # replace spaces & periods with hyphens
-      .gsub(/-{2,}/, "-") # replace two (or more) subsequent hyphens with single hyphens
+    Sanitize.fragment(string)
+      .downcase
+      .gsub(/&(amp|gt|lt);/, "")
+      .tr(" .", "-")
+      .tr("^a-z0-9_-", "")
+      .gsub(/--+/, "-")
   end
 end
