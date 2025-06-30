92664c16c99f041daaaedc155400076f4a6de575
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-06-30 11:40:48 +0100

    Revert "Ignore content_block_email_address schema"
    This reverts commit 8d9d7388e7aaf6a8a2fa1e60ed6370ef07ca40f1.
    
    Now that email_address_content_block is no longer a schema
    (email address is part of "contact"), we no longer need this
    work-around.

--- app/proxy_pages.rb
+++ app/proxy_pages.rb
@@ -37,7 +37,7 @@ class ProxyPages
   end
 
   def self.govuk_schema_names
-    SchemaNames.all.map do |schema_name|
+    GovukSchemas::Schema.schema_names.map do |schema_name|
       schema = ContentSchema.new(schema_name)
 
       {
